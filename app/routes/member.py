9b89bd7f662801d531c6c94b6129ab8d8d5efb1a
YeEul, Kwon <gruuriii@gmail.com>
2024-02-06 11:50:13 +0900

    회원가입 - 프론트엔드

--- app/routes/member.py
+++ app/routes/member.py
@@ -3,15 +3,24 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
+from app.schemas.member import NewMember
+
 member_router = APIRouter()
 
 # jinja2 설정
 templates = Jinja2Templates(directory='views/templates')
 member_router.mount('/static', StaticFiles(directory='views/static'), name='static')
 
+
 @member_router.get('/join', response_class=HTMLResponse)
 def join(req: Request):
     return templates.TemplateResponse('join.html', {'request': req})
+
+
+@member_router.post('/join')
+def joinok(req: Request, mdto: NewMember):
+    print(mdto)
+    return 1
 
 
 @member_router.get('/login', response_class=HTMLResponse)
